f637a4ed2f379bbdc9cd7dab3d85fc7240b23b94
Juan Cayuela <juancayuela@gmail.com>
2021-12-15 11:46:24 +0100

    cambios en los test

--- test/CalculadoraTest.php
+++ test/CalculadoraTest.php
@@ -4,10 +4,5 @@ namespace App\Test;
 use App\Calculadora;
 
 class CalculadoraTest extends \PHPUnit\Framework\TestCase {
-	public function testSuma1()
-    {
-        $calc = new calculadora(3, 4);
-        $this->assertEquals(7, $calc->suma());
-    }
 
 }
