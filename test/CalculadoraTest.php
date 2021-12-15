313e1d5a70d41dac1da4de94df3f4ce8efb4faf2
DESKTOP-M5UDMJM\Juan Carlos <juancarlosrodriguezlopez,jcr42@gmail.com>
2021-12-15 12:16:41 +0100

    Añadido Multiplicar

--- test/CalculadoraTest.php
+++ test/CalculadoraTest.php
@@ -8,4 +8,11 @@ class CalculadoraTest extends \PHPUnit\Framework\TestCase {
         $calc = new Calculadora(6, 10);
         $this->assertEquals(0.6, $calc->dividir());
     }
+
+    public function testMultiplicar(){
+        $calc = new Calculadora(9, 9);
+        $this->assertEquals(81, $calc->mulstiplicar());
+    }
+    
+
 }
