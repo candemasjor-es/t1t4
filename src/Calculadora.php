ff8918e578e505080e528976f2aa04b123642d3f
Juan Cayuela <juancayuela@gmail.com>
2021-12-15 11:30:35 +0100

    Primer commit

---
+++ src/Calculadora.php
@@ -0,0 +1,14 @@
+<?php
+namespace App;
+
+class calculadora
+{
+    private $num1;
+    private $num2;
+    public function __construct($a1, $a2)
+    {
+        $this->num1 = $a1;
+        $this->num2 = $a2;
+    }
+
+}
